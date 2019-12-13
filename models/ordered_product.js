175fe3e3d405291119df69781401e3896ad1ee17
Kamil Klasicki <kamil.klasicki@divido.com>
2019-12-13 09:51:52 +0000

    added some more comments

--- models/ordered_product.js
+++ models/ordered_product.js
@@ -2,12 +2,12 @@ const pushDataToKlaviyo = require('../utils/klaviyo');
 const getLogger = require('../utils/logger');
 
 const orderedProduct = (value) => {
-  value.line_items.forEach((item) => { // Since there can be multiple items for each order, I loop through each item to make sure I call Klaviyo for each item
+  value.line_items.forEach((item) => { // Since there can be multiple items for each order, I loop through each item to make sure I call Klaviyo for each item. Each order must have at least one item so it's safe to assume this payload will be mapped at least once for each order.
     const payload = {
       token: process.env.PUBLIC_API_KEY,
       event: 'Ordered Product',
       customer_properties: {
-        $email: value.email,
+        $email: value.email, //Email, first_name and last_name are always required when placing the order so we can be confident that they get returned from Shopify. Worst case that can happen is one of the values being set to undefined.
         $first_name: value.customer.first_name,
         $last_name: value.customer.last_name,
       },
